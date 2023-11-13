3679ded801a2467389d5538ebd4c582c6e683fdb
unknown <jbjabzin@gmail.com>
2023-11-12 23:18:41 -0600

    Working on player styling

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	export let data;
-	let expandedPosts = data.result.map(post => post.id);
+	// console.log('data: ', data);
+	let expandedPosts = data.result.map(post => post.path);
+	// console.log('expandedPosts: ', expandedPosts);
 
 	const toggleContent = postId => {
-		const index = number = expandedPosts.indexOf(postId);
+		const index = expandedPosts.indexOf(postId);
+		// console.log({index, postId})
 		if(index >= 0) {
 			expandedPosts.splice(index, 1);
 		} else {
@@ -16,8 +19,8 @@
 <div>
 	{#each data.result as post}
 		<div class='post_block'>
-			<h2 class='title' on:click={() => toggleContent(post.id)}>{post.title}</h2>
-			{#if expandedPosts.includes(post.id)}
+			<h2 class='title' on:click={() => toggleContent(post.path)}>{post.title}</h2>
+			{#if expandedPosts.includes(post.path)}
 				<div class='post_content'>{@html post.content}</div>
 			{/if}
 			<div></div>
--- src/routes/rosters/+page.svelte
+++ src/routes/rosters/+page.svelte
@@ -9,7 +9,26 @@ console.log(data)
 	{#each data.posts as post}
 		<div class="player_block">
 			<h2 class="IGN">{post.meta.gamerTag}</h2>
-			<!-- <p>{post.meta.IRL}</p> -->
+			<div class="player_content">
+				<div class="about">
+					<p>{post.meta.IRL}</p>
+					<p>{@html post.meta.about}</p>
+					<ul>
+						<!-- {#each post.meta.socialMedia as socialMedia}
+							<li>
+								<a href="{socialMedia.url}">{socialMedia.platform}</a>
+							</li>
+						{/each} -->
+					</ul>
+				</div>
+				<div class="esports">
+					<p>{post.meta.esportsTeamAndRole?.esportsTeam}</p>
+					<p>{post.meta.esportsTeamAndRole?.teamRoles[0]}</p>
+				</div>
+				<div class="games">
+					
+				</div>
+			</div>
 		</div>
 	{/each}
 </ul>
