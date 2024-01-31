653d097792f71901db66f82192bd0918ec78d13c
Jessica <36863810+FirebrickRed@users.noreply.github.com>
2024-01-31 15:31:30 +0000

    Added in base player carousel

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,12 +1,10 @@
 <script>
 	export let data;
-	// console.log('data: ', data);
+
 	let expandedPosts = data.result.map(post => post.path);
-	// console.log('expandedPosts: ', expandedPosts);
 
 	const toggleContent = postId => {
 		const index = expandedPosts.indexOf(postId);
-		// console.log({index, postId})
 		if(index >= 0) {
 			expandedPosts.splice(index, 1);
 		} else {
@@ -19,7 +17,10 @@
 <div>
 	{#each data.result as post}
 		<div class='post_block'>
-			<h2 class='title' on:click={() => toggleContent(post.path)}>{post.title}</h2>
+			<h2 class='title' 
+				on:click={() => toggleContent(post.path)}
+				on:keydown={() => toggleContent(post.path)}
+			>{post.title}</h2>
 			{#if expandedPosts.includes(post.path)}
 				<div class='post_content'>{@html post.content}</div>
 			{/if}
--- src/routes/rosters/+page.svelte
+++ src/routes/rosters/+page.svelte
@@ -1,6 +1,24 @@
 <script>
-export let data;
-console.log(data)
+	export let data;
+	const carouselTitles = ['About', 'Game'];
+	let carouselIndex = 0;
+
+	data.posts.forEach(post => {
+		post.carouselIndex = 0;
+	});
+
+	const handleCarousleClick = (post, i) => {
+		let newIndex = post.carouselIndex + i;
+		if(newIndex >= carouselTitles.length) {
+			newIndex = 0;
+		} else if (newIndex < 0) {
+			newIndex = carouselTitles.length - 1;
+		}
+		post.carouselIndex = newIndex;
+
+		data.posts = data.posts.map(p => p.path === post.path ? {...p, carouselIndex: newIndex} : p);
+	}
+	console.log(data);
 </script>
 
 <h1>Roster</h1>
@@ -10,23 +28,50 @@ console.log(data)
 		<div class="player_block">
 			<h2 class="IGN">{post.meta.gamerTag}</h2>
 			<div class="player_content">
-				<div class="about">
-					<p>{post.meta.IRL}</p>
-					<p>{@html post.meta.about}</p>
-					<ul>
-						<!-- {#each post.meta.socialMedia as socialMedia}
-							<li>
-								<a href="{socialMedia.url}">{socialMedia.platform}</a>
-							</li>
-						{/each} -->
-					</ul>
-				</div>
-				<div class="esports">
-					<p>{post.meta.esportsTeamAndRole?.esportsTeam}</p>
-					<p>{post.meta.esportsTeamAndRole?.teamRoles[0]}</p>
-				</div>
-				<div class="games">
-					
+
+				{#if post.carouselIndex === 0}
+					<div class="about">
+						<p>{post.meta.IRL}</p>
+						<p>{@html post.meta.about}</p>
+						<ul>
+							<!-- {#each post.meta.socialMedia as socialMedia}
+								<li>
+									<a href="{socialMedia.url}">{socialMedia.platform}</a>
+								</li>
+							{/each} -->
+						</ul>
+					</div>
+				{/if}
+	
+				{#if post.carouselIndex === 1}
+					<div class="games">
+						{#if post.meta.recentGame}
+							<p>Recent Game: {post.meta.recentGame}</p>
+						{/if}
+						{#if post.meta.favoriteGames}
+						<p>Favorite Games:</p>
+							<ol>
+								{#each post.meta.favoriteGames as favoriteGame}
+									<li>{favoriteGame}</li>
+								{/each}
+							</ol>
+						{/if}
+					</div>
+				{/if}
+
+				{#if post.carouselIndex === 2}
+					<div class="esports">
+						<p>{post.meta.esportsTeamAndRole?.esportsTeam}</p>
+						<p>{post.meta.esportsTeamAndRole?.teamRoles[0]}</p>
+					</div>
+				{/if}
+				
+			</div>
+			<div class='carouselControls'>
+				<div class='carouselButtons'>
+					<button on:click={() => handleCarousleClick(post, -1)}>Left</button>
+					<h3>{carouselTitles[post.carouselIndex]}</h3>
+					<button on:click={() => handleCarousleClick(post, 1)}>Right</button>
 				</div>
 			</div>
 		</div>
